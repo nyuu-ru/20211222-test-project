7bbd1067f9f6429dbee0ba67aa7fd9fa808bce9f
evilunyuu <u@nyuu.ru>
2021-12-22 17:36:07 +0500

    Начало~~

---
+++ github-test-project-1/main.cc
@@ -0,0 +1,17 @@
+/*
+ * main.cc
+ *
+ *  Created on: 22 дек. 2021 г.
+ *      Author: unyuu
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Ох жеж, оно работает~~" << std::endl;
+	return 0;
+}
+
+
+
